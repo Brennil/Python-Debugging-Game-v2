f395d43a7130bbc68f84038adf5e9b7e10fab024
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2025-11-12 11:34:46 +0800

    Delete constants.ts

--- constants.ts
+++
@@ -1,5 +0,0 @@
-
-export const TOTAL_QUESTIONS = 10;
-export const QUESTION_TIME_LIMIT_MS = 20000; // 20 seconds
-export const MAX_SCORE_PER_QUESTION = 100;
-export const MIN_SCORE_PER_QUESTION = 10;
