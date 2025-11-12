b24e44984667d6ac3d3f40b0456b71f05124981a
Jovita Tang <47584623+Brennil@users.noreply.github.com>
2025-11-12 09:44:42 +0800

    Create constants.ts

---
+++ constants.ts
@@ -0,0 +1,5 @@
+
+export const TOTAL_QUESTIONS = 10;
+export const QUESTION_TIME_LIMIT_MS = 20000; // 20 seconds
+export const MAX_SCORE_PER_QUESTION = 100;
+export const MIN_SCORE_PER_QUESTION = 10;
